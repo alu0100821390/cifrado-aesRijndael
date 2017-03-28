1e835f54ea672cd65a7dccee4fe3e69db053fa50
alu0100821390 <alu0100821390@ull.edu.es>
2017-03-28 10:51:14 +0100

    Actualizar encabezado

--- rijndael.py
+++ rijndael.py
@@ -1,17 +1,17 @@
 ##########################################################################################
-## Universidad de La Laguna						 										##
-## Escuela Superior de Ingeniería y Tecnología	 										##
-## Grado en Ingeniería Informática				 										##
-## Seguridad en Sistemas Informáticos			 										##
-## Fecha: 28/03/2016							 										##
-## Autor: Kevin Estévez Expósito (alu0100821390) 										##
-## 																						##
-## Práctica 5: Cifrado AES/Rijndael											 			##
-## Descripción: Cifrado y descifrado de mensajes mediante el cifrado AES/Rijndael.		##
-##											 											##
-## Ejecución: py rijndael.py															##
-## Ejemplo de clave: 000102030405060708090a0b0c0d0e0f									##
-## Ejemplo de bloque de texto original: 00112233445566778899aabbccddeeff				##
+## Universidad de La Laguna								##
+## Escuela Superior de Ingeniería y Tecnología	 					##
+## Grado en Ingeniería Informática				 			##
+## Seguridad en Sistemas Informáticos			 				##
+## Fecha: 28/03/2016									##
+## Autor: Kevin Estévez Expósito (alu0100821390) 					##
+## 											##
+## Práctica 5: Cifrado AES/Rijndael				 			##
+## Descripción: Cifrado y descifrado de mensajes mediante el cifrado AES/Rijndael.	##
+##											##
+## Ejecución: py rijndael.py								##
+## Ejemplo de clave: 000102030405060708090a0b0c0d0e0f					##
+## Ejemplo de bloque de texto original: 00112233445566778899aabbccddeeff		##
 ##########################################################################################
 
 
